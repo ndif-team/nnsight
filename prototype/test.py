bbe0ba2c70ade16c0109fb590eacef590b1f2e09
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2023-07-17 14:28:24 -0400

    no need for invoker variable in example test.py

--- prototype/test.py
+++ prototype/test.py
@@ -2,14 +2,14 @@ from .Model import Model
 
 model = Model('gpt2')
 
-with model.invoke('Hello world') as invoker:
+with model.invoke('Hello world'):
 
     mmlp0 = model.h[0].mlp.output.copy()
     mmlp1 = model.h[1].mlp.output + 2
 
     model.h[2].mlp.output = mmlp0 + mmlp1
 
-with model.invoke('Goodbye world') as invoker:
+with model.invoke('Goodbye world'):
 
     model.h[1].mlp.output = mmlp1
 
