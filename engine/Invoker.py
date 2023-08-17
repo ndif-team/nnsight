a631d8d0cf32ce59ee895a7758acf40e68d0d4f7
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2023-08-17 13:29:40 -0400

    Allow inputting a list of ints as tokens. Tokenizer needs padding on left side which complicates things. Users need to use .token() or .t() on a proxy to index on the token dimension.

--- engine/Invoker.py
+++ engine/Invoker.py
@@ -51,24 +51,32 @@ class Invoker:
 
     @override
     def __enter__(self) -> Invoker:
+        # Were in a new invocation so set generation_idx to 0
         self.state.generation_idx = 0
 
+        # Run graph_mode with meta tensors to collect shape information
         inputs = self.state.model.prepare_inputs(self.input)
         self.state.model.run_graph(inputs.copy(), *self.args, **self.kwargs)
 
+        # Decode tokenized inputs for use usage
         self.tokens = [
             self.state.model.tokenizer.decode(token) for token in inputs["input_ids"][0]
         ]
 
+        # Rebuild prompt from tokens (do this becuase if they input ids directly, we still need to pass
+        # all input data at once to a tokenizer to correctly batch the attention)
         self.state.prompts.append("".join(self.tokens))
 
         return self
 
     @override
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        # Exiting an invocation so if we enter a new one, it will be a new batch idx
         self.state.batch_idx += 1
 
     def next(self) -> None:
+        # .next() increases which generation idx the interventions happen
         self.state.generation_idx += 1
 
+        # Run graph with
         self.state.model.run_graph("_", *self.args, **self.kwargs)
