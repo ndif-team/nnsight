2fdfd4e33f2023a0eec8fb19c872f33e3db10a45
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2023-09-06 13:20:35 -0400

    README updates

--- examples/adhoc_module.py
+++ examples/adhoc_module.py
@@ -4,7 +4,7 @@ import torch
 model = Model("gpt2")
 
 with model.generate(device_map='cuda:0') as generator:
-    with generator.invoke('Hello world') as invoker:
+    with generator.invoke('The Eiffel Tower is in the city of') as invoker:
         
         hidden_states = model.transformer.h[-1].output[0]
         hidden_states = model.lm_head(model.transformer.ln_f(hidden_states)).save()
