e4e441692a5fbb1493bcf2e4587c7e3762382d35
JadenFiottoKaufman <JadenFK@outlook.com>
2023-10-25 21:30:34 -0400

    Documentation.

--- src/nnsight/models/__init__.py
+++ src/nnsight/models/__init__.py
@@ -45,7 +45,31 @@ GPT2LMHeadModel(
   (lm_head): Linear(in_features=768, out_features=50257, bias=False)
 )
 
-Models should inherit the AbstractModel class and implement the abstract methods
+The main two methods to interact and run the model are ``.generate(...)`` and ``.forward(...)``
 
+Both return context manager objects which when entered, track operations you perform on the inputs and outputs of modules.
 
+The generate context is meant for multi-iteration, and multi-input inference runs.
+Arguments passed to generate determine the generation behavior, in this case to generate three tokens.
+Within a generation context, invoker sub-contexts are entered using ``generator.invoke``. This is where input (or batch of inputs) to the model is accepted, and batched with other invocations.
+It's in these contexts where operations on inputs and outputs of modules are tracked and prepared for execution.
+
+In this example, we run two prompts on the language model in order to generate three tokens.
+We also perform a ``.save()`` operation on the output of the lm_head module, the logit outputs, in order to save these activations and access them after generation is over:
+
+>>> model.generate(max_new_tokens=3) as generator:
+>>>     generator.invoke("The Eiffel Tower is in the city of") as invoker:
+>>>         logits1 = model.lm_head.output.save()
+>>>     generator.invoke("The Empire State Building is in the city of") as invoker:
+>>>         logits2 = model.lm_head.output.save()
+>>> print(logits1.value)
+>>> print(logits2.value)
+>>> print(runner.output)
+
+The forward, runner context is meant for direct input to the underlying model (or module) in either inference or training mode where gradients are collected.
+
+>>> model.forward("The Eiffel Tower is in the city of", inference=True) as runner:
+>>>     logits = model.lm_head.output.save()
+>>> print(logits.value)
+>>> print(runner.output)
 """
