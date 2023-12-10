2d52373aa0f8c0999f8e823c00c85cb0334b1158
cadentj <caden.juang@gmail.com>
2023-12-10 01:09:54 -0500

    mostly finished tuned_lens and top_k test

--- src/nnsight/toolbox/lens/lens.py
+++ src/nnsight/toolbox/lens/lens.py
@@ -1,19 +1,26 @@
-import torch
+import torch as t
 from abc import ABC, abstractmethod
 
-from nnsight import LanguageModel, Module
+from nnsight import LanguageModel
+import nnsight
 from typing import Any, Callable, Dict, List, Union
-
+import json
 from functools import reduce
+from copy import deepcopy
+
+from .load_artifacts import load_lens_artifacts
 
 class Lens(ABC):
 
     def __init__(self):
         super().__init__()
-        self.tuned = False
         
     @abstractmethod
     def __call__(self) -> Any:
+        pass
+
+    @abstractmethod
+    def transform_hidden(self, hidden: t.Tensor) -> t.Tensor:
         pass
 
 class LogitLens(Lens):
@@ -22,42 +29,88 @@ class LogitLens(Lens):
     """
 
     def __init__(self, 
-                 layers: List[Module],
-                 decoding_modules: List[Module],
+                 layers: List[nnsight.Module],
+                 decoding_modules: List[nnsight.Module],
                 ) -> None:
         super().__init__()
-        
-        self.tuned = False
-        
+                
         self.layers = layers
         self.decoder = lambda x: reduce(lambda acc, func: func(acc), decoding_modules, x)
 
     def __call__(
             self,
-            indices: Union[int, List] = None,
-            as_probs: bool = True,
+            h: nnsight.Module,
         ) -> List[Any]:
-            
-        observations = []
         
-        for layer in self.layers:
-            logits = self.decoder(layer.output[0]) # apply decoder to hidden state
+        return self.decoder(h)
+    
+    def transform_hidden(self, h: t.Tensor) -> t.Tensor:
+        return h
 
-            observations.append(logits.save())
 
-        # Return logits over a specific token
-        if type(indices) == List or type(indices == int):
-            observations = [logits[:,indices,:] for logits in observations]
+import torch as t
+
+# class Unembed(t.nn.Module):
+#     pass
+
+class TunedLens(t.nn.Module):
+    """A tuned lens for decoding hidden states into logits."""
+
+    def __init__(
+        self,
+        model: nnsight.LanguageModel,
+    ):
+        """Create a TunedLens.
+
+        Args:
+            unembed: The unembed operation to use.
+            config: The configuration for this lens.
+        """
+        t.nn.Module.__init__(self)
+
+        self.model_name = model.config._name_or_path
+        config_path, self.ckpt_path = load_lens_artifacts(self.model_name)
+        with open(config_path, "r") as f:
+            self.config = json.load(f)
             
-        # Raw logits to probabilities
-        if as_probs:
-            observations = [logits.softmax(dim=-1) for logits in observations]
+        # unembed_hash = unembed.unembedding_hash()
+        # config.unembed_hash = unembed_hash
 
-        self.observations = observations
+        # The unembedding might be int8 if we're using bitsandbytes
+        dtype = model.config.torch_dtype
 
-class TunedLens(Lens):
+        translator = t.nn.Linear(
+            self.config['d_model'], self.config['d_model'], bias=self.config['bias'], dtype=dtype
+        )
+        translator.weight.data.zero_()
+        translator.bias.data.zero_()
 
-    def __init__(self):
-        super().__init__()
-        self.tuned = True
+        # Don't include the final layer since it does not need a translator
+        self.layer_translators = t.nn.ModuleList(
+            [deepcopy(translator) for _ in range(self.config['num_hidden_layers'])]
+        )
+
+    def __getitem__(self, item: int) -> t.nn.Module:
+        """Get the probe module at the given index."""
+        return self.layer_translators[item]
+
+    def __call__(self, h: nnsight.Module, idx: int):
+        """Transform and then decode the hidden states into logits."""
+        return self.transform_hidden(h, idx)
+    
+    def load_states(self) : 
+        state = t.load(self.ckpt_path)
+        self.layer_translators.load_state_dict(state)
+        print("<All keys matched successfully>")
+
+    def transform_hidden(self, h: nnsight.Module, idx: int) -> t.Tensor:
+        """Transform hidden state from layer `idx`."""
+        # Note that we add the translator output residually, i  n contrast to the formula
+        # in the paper. By parametrizing it this way we ensure that weight decay
+        # regularizes the transform toward the identity, not the zero transformation.
+        return h + self[idx](h)
+
+    def get(self):
+        return self[0]
+
 
---
+++ src/nnsight/toolbox/lens/load_artifacts.py
@@ -0,0 +1,134 @@
+"""Load lens artifacts from the hub or locally storage.
+
+Thanks to Belrose et. al for many pretrained lenses and a bulk of the code.
+
+See https://github.com/AlignmentResearch/tuned-lens for more.
+"""
+import os
+from pathlib import Path
+from typing import Optional
+from typing import Set, Tuple
+
+from huggingface_hub import HfFileSystem, hf_hub_download
+from huggingface_hub.utils import EntryNotFoundError
+
+
+def available_lens_artifacts(
+    repo_id: str,
+    repo_type: str,
+    revision: str = "main",
+    config_file: str = "config.json",
+    ckpt_file: str = "params.pt",
+    subfolder: str = "lens",
+) -> Set[str]:
+    """Get the available lens artifacts from the hub."""
+    fs = HfFileSystem()
+
+    repo_type = repo_type + "s" if not repo_type.endswith("s") else repo_type
+
+    root = Path(repo_type, repo_id, subfolder)
+    with_config = map(
+        Path,
+        fs.glob(
+            (root / "**" / config_file).as_posix(), revision=revision  # type: ignore
+        ),
+    )
+
+    with_pt = map(
+        Path,
+        fs.glob(
+            (root / "**" / ckpt_file).as_posix(), revision=revision  # type: ignore
+        ),
+    )
+    paths = {p.parent for p in with_pt}.intersection({p.parent for p in with_config})
+    return {p.relative_to(root).as_posix() for p in paths}
+
+
+def load_lens_artifacts(
+    resource_id: str,
+    repo_id: Optional[str] = None,
+    repo_type: Optional[str] = None,
+    revision: str = "main",
+    config_file: str = "config.json",
+    ckpt_file: str = "params.pt",
+    subfolder: str = "lens",
+    cache_dir: Optional[str] = None,
+) -> Tuple[Path, Path]:
+    """First checks for lens resource locally then tries to download it from the hub.
+
+    Args:
+        resource_id: The id of the lens resource.
+        repo_id: The repository to download the lens from. Defaults to
+            'AlignmentResearch/tuned-lens'. However, this default can be overridden by
+            setting the TUNED_LENS_REPO_ID environment variable.
+        repo_type: The type of repository to download the lens from. Defaults to
+            'space'. However, this default can be overridden by setting the
+            TUNED_LENS_REPO_TYPE environment variable.
+        config_file: The name of the config file in the folder contain the lens.
+        ckpt_file: The name of the checkpoint file in the folder contain the lens.
+        revision: The revision of the lens to download.
+        subfolder: The subfolder of the repository to download the lens from.
+        cache_dir: The directory to cache the lens in.
+
+    Returns:
+        * The path to the config.json file
+        * The path to the params.pt file
+
+    Raises:
+        ValueError: if the lens resource could not be found.
+    """
+    if repo_id is None:
+        if os.environ.get("TUNED_LENS_REPO_ID"):
+            repo_id = os.environ["TUNED_LENS_REPO_ID"]
+        else:
+            repo_id = "AlignmentResearch/tuned-lens"
+
+    if repo_type is None:
+        if os.environ.get("TUNED_LENS_REPO_TYPE"):
+            repo_type = os.environ["TUNED_LENS_REPO_TYPE"]
+        else:
+            repo_type = "space"
+
+    # Fist check if the resource id is a path to a folder that exists
+    local_path = Path(resource_id)
+    if (local_path / config_file).exists() and (local_path / ckpt_file).exists():
+        return local_path / config_file, local_path / ckpt_file
+
+    resource_folder = "/".join((subfolder, resource_id))
+    try:
+        params_path = hf_hub_download(
+            filename=ckpt_file,
+            repo_id=repo_id,
+            repo_type=repo_type,
+            revision=revision,
+            subfolder=resource_folder,
+            cache_dir=cache_dir,
+        )
+
+        config_path = hf_hub_download(
+            filename=config_file,
+            repo_id=repo_id,
+            repo_type=repo_type,
+            revision=revision,
+            subfolder=resource_folder,
+            cache_dir=cache_dir,
+        )
+    except EntryNotFoundError:
+        available_lenses = available_lens_artifacts(
+            repo_id=repo_id,
+            repo_type=repo_type,
+            revision=revision,
+            config_file=config_file,
+            ckpt_file=ckpt_file,
+            subfolder=subfolder,
+        )
+        message = (
+            f"Could not find lens at the specified resource id. Available lens"
+            f"resources are: {', '.join(available_lenses)}"
+        )
+        raise ValueError(message)
+
+    if config_path is not None and params_path is not None:
+        return Path(config_path), Path(params_path)
+
+    raise ValueError("Could not find lens resource locally or on the hf hub.")
