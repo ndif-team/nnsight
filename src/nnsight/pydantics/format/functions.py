8d4de2f97318cd554ec862a41ccceec40be9a57d
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2024-01-04 14:17:03 -0500

    Custom serialization format. Dropping pickle on the request side

---
+++ src/nnsight/pydantics/format/functions.py
@@ -0,0 +1,38 @@
+import operator
+from inspect import getmembers, isbuiltin, ismethoddescriptor
+
+import torch
+
+from ... import util
+from ...tracing.Proxy import Proxy
+
+FUNCTIONS_WHITELIST = {}
+FUNCTIONS_WHITELIST.update(
+    {
+        f"_VariableFunctionsClass.{key}": value
+        for key, value in getmembers(torch._C._VariableFunctions, isbuiltin)
+    }
+)
+FUNCTIONS_WHITELIST.update(
+    {
+        f"Tensor.{key}": value
+        for key, value in getmembers(torch._C._TensorBase, ismethoddescriptor)
+    }
+)
+FUNCTIONS_WHITELIST.update(
+    {
+        f"{key}": value
+        for key, value in getmembers(operator, isbuiltin)
+        if not key.startswith("_")
+    }
+)
+FUNCTIONS_WHITELIST.update(
+    {
+        "null": "null",
+        "module": "module",
+        "argument": "argument",
+        "swp": "swp",
+        "fetch_attr": util.fetch_attr,
+        "Proxy.proxy_call": Proxy.proxy_call,
+    }
+)
