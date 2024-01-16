3a3be2056f6f9160f51ec208d0c8da1708e84397
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2024-01-16 12:29:45 -0500

    NDIF request comments and edits for latest ndif version

--- src/nnsight/contexts/Runner.py
+++ src/nnsight/contexts/Runner.py
@@ -84,8 +84,8 @@ class Runner(Tracer):
         )
 
     def run_server(self):
-        # Create the pydantic class for the request.
 
+        # Create the pydantic object for the request.
         request = pydantics.RequestModel(
             args=self.args,
             kwargs=self.kwargs,
@@ -123,14 +123,18 @@ class Runner(Tracer):
 
             # If the status of the response is completed, update the local nodes that the user specified to save.
             # Then disconnect and continue.
-
             if response.status == pydantics.ResponseModel.JobStatus.COMPLETED:
+
+                # Create BytesIO object to store bytes received from server in.
                 result_bytes = io.BytesIO()
                 result_bytes.seek(0)
 
+                # Get result from result url using job id.
                 with requests.get(
                     url=f"https://{CONFIG.API.HOST}/result/{response.id}", stream=True
                 ) as stream:
+                    
+                    # Total size of incoming data.
                     total_size = float(stream.headers["Content-length"])
 
                     with tqdm(
@@ -139,22 +143,30 @@ class Runner(Tracer):
                         unit_scale=True,
                         desc="Downloading result",
                     ) as progress_bar:
-                        for data in stream.iter_content(chunk_size=4000000):
+                        # chunk_size=None so server determines chunk size.
+                        for data in stream.iter_content(chunk_size=None):
                             progress_bar.update(len(data))
                             result_bytes.write(data)
 
+                # Move cursor to beginning of bytes.
                 result_bytes.seek(0)
 
+                # Decode bytes with pickle and then into pydantic object.
                 result = pydantics.ResultModel(**pickle.load(result_bytes))
 
+                # Close bytes
                 result_bytes.close()
 
+                # Set save data.
                 for name, value in result.saves.items():
                     self.graph.nodes[name].value = value
 
+                # Set output data.
                 self.output = result.output
 
+                # Disconnect and continue program.
                 sio.disconnect()
+
             # Or if there was some error.
             elif response.status == pydantics.ResponseModel.JobStatus.ERROR:
                 sio.disconnect()
