27446a8478895f5e12ac913a9f29c3e7ef842a1a
cadentj <caden.juang@gmail.com>
2024-01-26 17:52:30 -0500

    UnifiedTransformer

--- src/nnsight/models/UnifiedTransformer.py
+++ src/nnsight/models/UnifiedTransformer.py
@@ -27,45 +27,33 @@ class UnifiedTransformer(NNsightModel):
     Calls to generate pass arguments downstream to :func:`GenerationMixin.generate`
 
     Attributes:
-        config (PretrainedConfig): Huggingface config file loaded from repository or checkpoint.
+        config (HookedTransformerConfig): Huggingface config file loaded from repository or checkpoint.
         tokenizer (PreTrainedTokenizer): Tokenizer for LMs.
-        automodel (type): AutoModel type from transformer auto models.
         meta_model (PreTrainedModel): Meta version of underlying auto model.
-        local_model (PreTrainedModel): Local version of underlying auto model.
+        local_model (HookedTransformer): Local version of underlying HookedTransformer.
 
     """
 
     def __init__(
-        self, *args, tokenizer=None, automodel=AutoModelForCausalLM, **kwargs
+        self, 
+        *args, 
+        device: str, 
+        **kwargs
     ) -> None:
-        self.config: PretrainedConfig = None
-        self.tokenizer: PreTrainedTokenizer = tokenizer
         self.meta_model: PreTrainedModel = None
         self.local_model: PreTrainedModel = None
-        self.automodel = (
-            automodel
-            if not isinstance(automodel, str)
-            else getattr(modeling_auto, automodel)
-        )
 
         super().__init__(*args, **kwargs)
 
+        self.tokenizer = self.local_model.tokenizer
+        self.config = self.local_model.cfg
+        self.local_model.device = device
+
     def _load_meta(self, repoid_or_path, *args, **kwargs) -> PreTrainedModel:
-        self.config = AutoConfig.from_pretrained(repoid_or_path, *args, **kwargs)
-
-        if self.tokenizer is None:
-
-            self.tokenizer = AutoTokenizer.from_pretrained(
-                repoid_or_path, config=self.config, padding_side="left"
-            )
-            self.tokenizer.pad_token = self.tokenizer.eos_token
-
-        return self.automodel.from_config(self.config, trust_remote_code=True)
+        raise NotImplementedError("This subclass does not implement this method.")
 
     def _load_local(self, repoid_or_path, *args, **kwargs) -> PreTrainedModel:
-        return self.automodel.from_pretrained(
-            repoid_or_path, *args, config=self.config, **kwargs
-        )
+        raise NotImplementedError("This subclass does not implement this method.")
 
     def _tokenize(
         self,
@@ -109,7 +97,6 @@ class UnifiedTransformer(NNsightModel):
             Dict[str, Any],
             BatchEncoding,
         ],
-        labels: Any = None,
         **kwargs,
     ) -> BatchEncoding:
         if isinstance(inputs, dict):
@@ -126,19 +113,9 @@ class UnifiedTransformer(NNsightModel):
 
                 new_inputs["attention_mask"] = tokenized_inputs["attention_mask"]
 
-            if "labels" in inputs:
-                labels = self._tokenize(inputs["labels"], **kwargs)
-
-                new_inputs["labels"] = labels["input_ids"]
-
             return BatchEncoding(new_inputs)
 
         inputs = self._tokenize(inputs, **kwargs)
-
-        if labels is not None:
-            labels = self._tokenize(labels, **kwargs)
-
-            inputs["labels"] = labels["input_ids"]
         
         if "input_ids" in inputs:
             inputs["input"] = inputs.pop("input_ids")
@@ -151,16 +128,11 @@ class UnifiedTransformer(NNsightModel):
         if batched_inputs is None:
             batched_inputs = {"input": []}
 
-            if "labels" in prepared_inputs:
-                batched_inputs["labels"] = []
-
             if "attention_mask" in prepared_inputs:
                 batched_inputs["attention_mask"] = []
 
         batched_inputs["input"].extend(prepared_inputs["input"])
 
-        if "labels" in prepared_inputs:
-            batched_inputs["labels"].extend(prepared_inputs["labels"])
         if "attention_mask" in prepared_inputs:
             batched_inputs["attention_mask"].extend(prepared_inputs["attention_mask"])
 
@@ -175,6 +147,7 @@ class UnifiedTransformer(NNsightModel):
         self, prepared_inputs, *args, max_new_tokens: int = 1, **kwargs
     ) -> Any:
 
+        # HookedTransformer uses attention_mask in forward, but not generate
         if "attention_mask" in prepared_inputs:
             prepared_inputs.pop("attention_mask")
 
