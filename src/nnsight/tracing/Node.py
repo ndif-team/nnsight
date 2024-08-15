0682f648de6ec24a8dd8cd4440207314a6e7db5e
Adam Belfki <belfki.adam@gmail.com>
2024-08-15 15:37:42 -0400

    fix

--- src/nnsight/tracing/Node.py
+++ src/nnsight/tracing/Node.py
@@ -393,7 +393,6 @@ class Node:
             bridge = protocols.BridgeProtocol.get_bridge(self.graph)
             lock_node = bridge.get_graph(self.args[0]).nodes[self.args[1]]
             lock_dependency = lock_node.args[0]
-            # if not lock_node.done():
             lock_dependency.remaining_listeners -= 1
             lock_node.destroy()
             if lock_dependency.redundant():
