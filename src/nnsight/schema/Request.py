47758d6c8acafc4825a9e811162060aa1aeafcf6
jadenfk@outlook.com <jadenfk@outlook.com>
2024-09-20 16:17:19 -0400

    Bug fix from last PR

--- src/nnsight/schema/Request.py
+++ src/nnsight/schema/Request.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from datetime import datetime
 import json
+from datetime import datetime
 from typing import TYPE_CHECKING, Dict, List, Union
 
-from pydantic import BaseModel, ConfigDict, field_serializer, TypeAdapter
+from pydantic import BaseModel, ConfigDict, TypeAdapter, field_serializer
 
 from .. import NNsight
 from .format.types import *
@@ -14,9 +14,12 @@ if TYPE_CHECKING:
 
 OBJECT_TYPES = Union[SessionType, TracerType, SessionModel, TracerModel]
 
+
 class RequestModel(BaseModel):
-    
-    model_config = ConfigDict(arbitrary_types_allowed=True, protected_namespaces=())
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True, protected_namespaces=()
+    )
 
     object: str | OBJECT_TYPES
     model_key: str
@@ -25,16 +28,23 @@ class RequestModel(BaseModel):
     received: datetime = None
 
     session_id: Optional[str] = None
-    
-    @field_serializer('object')
-    def serialize_object(self, object: Union[SessionType, TracerType, SessionModel, TracerModel]) -> str:
-        
+
+    @field_serializer("object")
+    def serialize_object(
+        self, object: Union[SessionType, TracerType, SessionModel, TracerModel]
+    ) -> str:
+
+        if isinstance(object, str):
+            return object
+
         return object.model_dump_json()
 
     def deserialize(self, model: NNsight) -> "RemoteMixin":
-        
+
         handler = DeserializeHandler(model=model)
-        
-        object = TypeAdapter(OBJECT_TYPES, config = RequestModel.model_config).validate_python(json.loads(self.object))
-        
+
+        object = TypeAdapter(
+            OBJECT_TYPES, config=RequestModel.model_config
+        ).validate_python(json.loads(self.object))
+
         return object.deserialize(handler)
