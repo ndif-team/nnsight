4ca671833f394eea8f6b103730ba03f71557cc87
jadenfk@outlook.com <jadenfk@outlook.com>
2024-12-05 14:31:22 -0500

    For value injection, need to tell the frame to update its locals

--- src/nnsight/tracing/backends/base.py
+++ src/nnsight/tracing/backends/base.py
@@ -26,14 +26,17 @@ class ExecutionBackend(Backend):
             if self.injection:
                 
                 from ..contexts import Context
+                import ctypes
 
                 frame = inspect.currentframe().f_back
                 while frame.f_back is not None and 'self' in frame.f_locals and isinstance(frame.f_locals['self'], Context):
                     frame = frame.f_back
-                                        
+                    
                 for key, value in frame.f_locals.items():
                     if isinstance(value, Proxy) and value.node.done:
                         frame.f_locals[key] = value.value
+                        
+                ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(frame), 0)
 
         except StopProtocol.StopException:
 
