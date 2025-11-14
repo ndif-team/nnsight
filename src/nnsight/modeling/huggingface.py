8c718ddf0dfc87360631570af2ecccd1e464e725
jadenfk@outlook.com <jadenfk@outlook.com>
2025-11-14 13:30:11 -0500

    Reorg

--- src/nnsight/modeling/huggingface.py
+++ src/nnsight/modeling/huggingface.py
@@ -1,16 +1,111 @@
-from typing import Optional
+from typing import Optional, Union
 from .mixins import RemoteableMixin
+import json
+from huggingface_hub import HfApi
+from typing_extensions import Self
+from huggingface_hub.file_download import repo_folder_name
+import os
+from huggingface_hub import constants
+
 
 class HuggingFaceModel(RemoteableMixin):
-    
-    
-    def __init__(self, repo_id: str, *args, revision: Optional[str] = None, **kwargs):
-        
-        self.repo_id = repo_id
+
+    def __init__(
+        self,
+        repo_id: str,
+        *args,
+        revision: Optional[str] = None,
+        import_edits: Union[bool, str] = False,
+        **kwargs,
+    ):
+
+        self.repo_id = (
+            repo_id
+            if isinstance(repo_id, str)
+            else getattr(repo_id, "name_or_path", None)
+        )
         self.revision = revision
-        
-        
+
         super().__init__(repo_id, *args, revision=revision, **kwargs)
-        
-        
-        
+
+        if import_edits:
+
+            if isinstance(import_edits, str):
+
+                self.import_edits(variant=import_edits)
+
+            else:
+
+                self.import_edits()
+
+    def export_edits(
+        self,
+        name: Optional[str] = None,
+        export_dir: Optional[str] = None,
+        variant: str = "__default__",
+    ):
+        """TODO
+
+        Args:
+            name (Optional[str], optional): _description_. Defaults to None.
+            export_dir (Optional[str], optional): _description_. Defaults to None.
+            variant (str, optional): _description_. Defaults to '__default__'.
+        """
+
+        if name is None:
+            name = repo_folder_name(repo_id=self.repo_id, repo_type="model")
+
+            if export_dir is None:
+                export_dir = os.path.join(
+                    constants.HF_HUB_CACHE, name, "nnsight", "exports"
+                )
+                name = ""
+
+        super().export_edits(name, export_dir=export_dir, variant=variant)
+
+    def import_edits(
+        self,
+        name: Optional[str] = None,
+        export_dir: Optional[str] = None,
+        variant: str = "__default__",
+    ):
+        """TODO
+
+        Args:
+            name (Optional[str], optional): _description_. Defaults to None.
+            export_dir (Optional[str], optional): _description_. Defaults to None.
+            variant (str, optional): _description_. Defaults to '__default__'.
+        """
+
+        if name is None:
+            name = repo_folder_name(repo_id=self.repo_id, repo_type="model")
+
+            if export_dir is None:
+                export_dir = os.path.join(
+                    constants.HF_HUB_CACHE, name, "nnsight", "exports"
+                )
+                name = ""
+
+        super().import_edits(name, export_dir=export_dir, variant=variant)
+
+    def _remoteable_model_key(self) -> str:
+
+        repo_id = HfApi().model_info(self.repo_id).id
+
+        return json.dumps(
+            {
+                "repo_id": repo_id,
+                "revision": self.revision,
+            }  # , "torch_dtype": str(self._model.dtype)}
+        )
+
+    @classmethod
+    def _remoteable_from_model_key(cls, model_key: str, **kwargs) -> Self:
+
+        kwargs = {**json.loads(model_key), **kwargs}
+
+        repo_id = kwargs.pop("repo_id")
+
+        revision = kwargs.pop("revision", "main")
+
+        return cls(repo_id, revision=revision, **kwargs)
--- src/nnsight/modeling/transformers.py
+++ src/nnsight/modeling/transformers.py
@@ -1,87 +1,61 @@
 from .huggingface import HuggingFaceModel
-import torch
-from huggingface_hub import HfApi, constants
-from huggingface_hub.file_download import repo_folder_name
+
 from torch.nn.modules import Module
-from transformers import (AutoConfig, AutoModel, AutoModelForCausalLM,
-                          AutoTokenizer, BatchEncoding, PretrainedConfig,
-                          PreTrainedModel, PreTrainedTokenizer)
-from typing import Optional, Self
-from transformers.models.llama.configuration_llama import LlamaConfig
-from typing_extensions import Self
-import json
+from transformers import AutoConfig, PreTrainedModel, PretrainedConfig
+from typing import Optional
+from typing import Type
+from transformers.models.auto import modeling_auto
+from transformers import AutoModel
+
 
 class TransformersModel(HuggingFaceModel):
-    
+
+    def __init__(self, *args, config_model: Type[PretrainedConfig] = None, automodel: Type[AutoModel] = AutoModel, **kwargs):
+
+        self.config: PretrainedConfig = config_model
+        
+        self.automodel = (
+            automodel
+            if not isinstance(automodel, str)
+            else getattr(modeling_auto, automodel)
+        )
+
+        super().__init__(*args, **kwargs)
+
+    def _load_config(self, repo_id: str, revision: Optional[str] = None, **kwargs):
+
+        if self.config is None:
+
+            self.config = AutoConfig.from_pretrained(
+                repo_id, revision=revision, **kwargs
+            )
+
     def _load_meta(
         self,
         repo_id: str,
-        revision:Optional[str] = None,
-        patch_llama_scan: bool = True,
+        revision: Optional[str] = None,
         **kwargs,
-    ) -> PreTrainedModel:
+    ) -> Module:
 
         self._load_config(repo_id, revision=revision, **kwargs)
 
-
-        if (
-            patch_llama_scan
-            and isinstance(self.config, LlamaConfig)
-            and isinstance(self.config.rope_scaling, dict)
-            and "rope_type" in self.config.rope_scaling
-        ):
-            self.config.rope_scaling["rope_type"] = "default"
-
         model = self.automodel.from_config(self.config, trust_remote_code=True)
-        
+
         self.config = model.config
-        
-        self._patch_generation_config(model)
 
         return model
-    
+
     def _load(
         self,
         repo_id: str,
-        revision:Optional[str] = None,
-        patch_llama_scan: bool = True,
+        revision: Optional[str] = None,
         **kwargs,
     ) -> PreTrainedModel:
 
         self._load_config(repo_id, revision=revision, **kwargs)
 
-        if (
-            patch_llama_scan
-            and isinstance(self.config, LlamaConfig)
-            and isinstance(self.config.rope_scaling, dict)
-            and "rope_type" in self.config.rope_scaling
-        ):
-            self.config.rope_scaling["rope_type"] = "llama3"
+        model = self.automodel.from_pretrained(repo_id, revision=revision, **kwargs)
 
-        model = self.automodel.from_pretrained(repo_id, config=self.config, revision=revision, **kwargs)
-        
         self.config = model.config
-        
-        self._patch_generation_config(model)
-        
+
         return model
-    
-    
-    def _remoteable_model_key(self) -> str:
-        
-        repo_id = HfApi().model_info(self.repo_id).id
-        
-        return json.dumps(
-            {"repo_id": repo_id, "revision": self.revision}  # , "torch_dtype": str(self._model.dtype)}
-        )
-
-    @classmethod
-    def _remoteable_from_model_key(cls, model_key: str, **kwargs) -> Self:
-
-        kwargs = {**json.loads(model_key), **kwargs}
-
-        repo_id = kwargs.pop("repo_id")
-
-        revision = kwargs.pop("revision", "main")
-
-        return LanguageModel(repo_id, revision=revision, **kwargs)
